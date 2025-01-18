25d125684f3577f03df4095174d11456c4ba9565
Avcatalin <avarvareicatalin@gmail.com>
2025-01-18 14:40:51 +0200

    updated js and video btn

--- js/main.js
+++ js/main.js
@@ -1,119 +1,137 @@
-// Modal functionality to show/hide and stop video if user clicks outside or closes the video
-const videoButton = document.getElementById("videoButton");
-const videoModal = document.getElementById("videoModal");
-const closeModalButton = document.getElementById("closeModalButton");
-const iframe = videoModal.querySelector("iframe"); // Get the iframe element
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleClasses = (element, classesToAdd, classesToRemove) => {
+    element.classList.remove(...classesToRemove);
+    element.classList.add(...classesToAdd);
+  };
+  
+  // Modal functionality to show/hide and stop video if user clicks outside or closes the video
+  const videoButton = document.getElementById("videoButton");
+  const videoModal = document.getElementById("videoModal");
+  const closeModalButton = document.getElementById("closeModalButton");
+  const iframe = videoModal.querySelector("iframe"); // Get the iframe element
 
-// Show the modal
-videoButton.addEventListener("click", () => {
-  videoModal.classList.remove("opacity-0", "invisible");
-  videoModal.classList.add("opacity-100", "visible");
-});
+  // Show the modal
+  videoButton.addEventListener("click", () => {
+    toggleClasses(
+      videoModal,
+      ["opacity-100", "visible"],
+      ["opacity-0", "invisible"]
+    );
 
-// Function to stop the video and hide the modal
-const closeModal = () => {
-  videoModal.classList.remove("opacity-100", "visible");
-  videoModal.classList.add("opacity-0", "invisible");
-  // Stop the video by resetting the iframe's src
-  iframe.src = iframe.src;
-};
+  });
 
-// Hide the modal when clicking the close button
-closeModalButton.addEventListener("click", closeModal);
+  // Function to stop the video and hide the modal
+  const closeModal = () => {
+    toggleClasses(
+      videoModal,
+      ["opacity-0", "invisible"],
+      ["opacity-100", "visible"]
+    );
+    // Stop the video by resetting the iframe's src
+    iframe.src = iframe.src;
+  };
 
-// Hide the modal and stop the video when clicking outside the video
-videoModal.addEventListener("click", (event) => {
-  if (event.target === videoModal) {
-    closeModal();
-  }
-});
+  // Hide the modal when clicking the close button
+  closeModalButton.addEventListener("click", closeModal);
 
-// Language code for showing the selected language
-const languageDropdown = document.getElementById("language-dropdown");
-const currentLanguageLabel = document.getElementById("currentLanguage");
+  // Hide the modal and stop the video when clicking outside the video
+  videoModal.addEventListener("click", (event) => {
+    if (event.target === videoModal) {
+      closeModal();
+    }
+  });
 
-// Add event listener for language selection
-languageDropdown.addEventListener("click", (event) => {
-  // Check if the clicked element is a language option
-  if (event.target.hasAttribute("data-language")) {
-    const selectedLanguage = event.target.getAttribute("data-language");
+  // Language code for showing the selected language
+  const languageDropdown = document.getElementById("language-dropdown");
+  const currentLanguageLabel = document.getElementById("currentLanguage");
 
-    // Update the current language label
-    currentLanguageLabel.textContent = selectedLanguage;
+  // Add event listener for language selection
+  languageDropdown.addEventListener("click", (event) => {
+    // Check if the clicked element is a language option
+    if (event.target.hasAttribute("data-language")) {
+      const selectedLanguage = event.target.getAttribute("data-language");
 
-    // Hide the dropdown menu
-    languageDropdown.classList.add("hidden");
-  }
-});
+      // Update the current language label
+      currentLanguageLabel.textContent = selectedLanguage;
 
-// Show/hide search input on keydown and click events
-const searchForm = document.getElementById("search-form");
-const closeFormButton = document.getElementById("closeFormButton");
-const searchTrigger = document.getElementById("search-trigger");
-const languageSwitcher = document.getElementById("languageTrigger");
+      // Hide the dropdown menu
+      languageDropdown.classList.add("hidden");
+    }
+  });
 
-// Function to show the form
-const showForm = () => {
-  languageSwitcher.classList.add("pr-0", "sm:pr-36", "md:pr-24");
-  languageDropdown.classList.remove("right-[-5px]");
-  languageDropdown.classList.add("right-auto", "left-[-4px]");
-  searchForm.classList.remove("opacity-0", "invisible");
-  searchForm.classList.add("opacity-100", "visible");
+  // Show/hide search input on keydown and click events
+  const searchForm = document.getElementById("search-form");
+  const closeFormButton = document.getElementById("closeFormButton");
+  const searchTrigger = document.getElementById("search-trigger");
+  const languageSwitcher = document.getElementById("languageTrigger");
 
-  // Delay focus until the form becomes fully visible
-  setTimeout(() => {
-    searchForm.querySelector("input").focus();
-  }, 50); // Adjust the timeout if necessary
-};
+  // Function to show the form
+  const showForm = () => {
+    languageSwitcher.classList.add("pr-0", "sm:pr-36", "md:pr-24");
+    toggleClasses(
+      searchForm,
+      ["opacity-100", "visible"],
+      ["opacity-0", "invisible"]
+    );
 
-// Function to hide the form
-const hideForm = () => {
-  languageSwitcher.classList.remove("pr-0", "sm:pr-36", "md:pr-24");
-  searchForm.classList.remove("opacity-100", "visible");
-  searchForm.classList.add("opacity-0", "invisible");
-};
+    // Delay focus until the form becomes fully visible
+    setTimeout(() => {
+      searchForm.querySelector("input").focus();
+    }, 50);
+  };
 
-// Show form when the search trigger is clicked
-searchTrigger.addEventListener("click", (event) => {
-  event.preventDefault(); // Prevent default anchor behavior
-  showForm();
-});
+  // Function to hide the form
+  const hideForm = () => {
+    languageSwitcher.classList.remove("pr-0", "sm:pr-36", "md:pr-24");
+    toggleClasses(
+      searchForm,
+      ["opacity-0", "invisible"],
+      ["opacity-100", "visible"]
+    );
+  };
 
-// Add keydown listener for ALT + K, Command + K, and ESC
-document.addEventListener("keydown", (event) => {
-  // Show form on ALT + K or Command + K
-  if (
-    (event.altKey && event.key === "k") ||
-    (event.metaKey && event.key === "k")
-  ) {
-    event.preventDefault(); // Prevent default browser behavior
+  // Show form when the search trigger is clicked
+  searchTrigger.addEventListener("click", (event) => {
+    event.preventDefault(); // Prevent default anchor behavior
     showForm();
-  }
+  });
 
-  // Hide form on ESC key
-  if (event.key === "Escape") {
-    hideForm();
-  }
-});
+  // Add keydown listener for ALT + K, Command + K, and ESC
+  document.addEventListener("keydown", (event) => {
+    // Show form on ALT + K or Command + K
+    if (
+      (event.altKey && event.key === "k") ||
+      (event.metaKey && event.key === "k")
+    ) {
+      event.preventDefault(); // Prevent default browser behavior
+      showForm();
+    }
 
-// Hide form when the close button is clicked
-closeFormButton.addEventListener("click", hideForm);
+    // Hide form on ESC key
+    if (event.key === "Escape") {
+      hideForm();
+    }
+  });
 
-// Hide form when clicking outside the form
-document.addEventListener("click", (event) => {
-  // Check if the click is outside the form and search trigger
-  if (
-    !searchForm.contains(event.target) &&
-    !searchTrigger.contains(event.target)
-  ) {
-    hideForm();
-  }
-});
+  // Hide form when the close button is clicked
+  closeFormButton.addEventListener("click", hideForm);
 
-// Mobile menu trigger
-const menuToggle = document.getElementById("menuToggle");
-const menu = document.getElementById("menu");
+  // Hide form when clicking outside the form
+  document.addEventListener("click", (event) => {
+    // Check if the click is outside the form and search trigger
+    if (
+      !searchForm.contains(event.target) &&
+      !searchTrigger.contains(event.target)
+    ) {
+      hideForm();
+    }
+  });
 
-menuToggle.addEventListener("click", () => {
-  menu.classList.toggle("hidden"); // Show or hide the menu
-});
+  // Mobile menu trigger
+  const menuToggle = document.getElementById("menuToggle");
+  const menu = document.getElementById("menu");
+
+  menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("hidden"); // Show or hide the menu
+  });
+})
